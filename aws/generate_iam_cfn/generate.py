a4bbc959d18d9890940ae343a65be15d13e92d1d
Josh Jaffe <josjaffe@amazon.com>
2018-09-28 13:54:05 -0400

    getting api calls from client

--- aws/generate_iam_cfn/generate.py
+++ aws/generate_iam_cfn/generate.py
@@ -1,20 +1,17 @@
+import boto3
 import re
 
-api_calls = """accept_reserved_instances_exchange_quote()
-accept_vpc_endpoint_connections()
-accept_vpc_peering_connection()
-allocate_address()"""
+ec2 = boto3.client('ec2')
+api_calls = dir(ec2)
 
+def snake_to_camel(word):
+    import re
+    return ''.join(x.capitalize() or '_' for x in word.split('_'))
 
-for line in api_calls.splitlines():
-    removed_underscores = re.sub(r"[()]", "", line)
-    word_list = removed_underscores.split("_")
-    final = ''
-    for word in word_list:
-        final = final + word.capitalize()
-    final = '- ec2:' + final     
+for line in api_calls:
+    if line.startswith("_"):
+        continue
+    if line.startswith("waiter"):
+        continue
+    final = "- ec2:{}".format(line)     
     print(final)
-
-
-# copy and paste from the client section
-#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#client
