931443bde62cf85e1b72718ed315998f6ff5f274
Josh Jaffe <josjaffe@amazon.com>
2018-09-28 15:44:09 -0400

    removing bad import for generate

--- aws/generate_iam_cfn/generate.py
+++ aws/generate_iam_cfn/generate.py
@@ -5,13 +5,10 @@ ec2 = boto3.client('ec2')
 api_calls = dir(ec2)
 
 def snake_to_camel(word):
-    import re
     return ''.join(x.capitalize() or '_' for x in word.split('_'))
 
 for line in api_calls:
-    if line.startswith("_"):
-        continue
-    if line.startswith("waiter"):
-        continue
+    if line.startswith("_"): continue
+    if line.startswith("waiter"): continue
     final = "- ec2:{}".format(line)     
     print(final)
