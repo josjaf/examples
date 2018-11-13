b4e85fef3d44f333773f50abc5adb3b9c09dcee1
Josh Jaffe <josjaffe@amazon.com>
2018-11-13 13:14:34 -0500

    helpers for jmespath stacks.py

--- aws/jmespath/stacks.py
+++ aws/jmespath/stacks.py
@@ -1,5 +1,8 @@
 import boto3
 import jmespath
+from helpers import text
+
+text_helpers = text.helpers()
 
 session = boto3.Session()
 cloudformation = session.client('cloudformation')
@@ -8,9 +11,13 @@ response = cloudformation.list_stacks()
 ids = jmespath.search("StackSummaries[].StackId", response)
 stacks = jmespath.search("StackSummaries[].StackName", response)
 
+
+text_helpers.print_separator("String Example")
 for stack in stacks:
     print(stack)
 
+
+text_helpers.print_separator("Dictionary Example")
 
 stacks = jmespath.search("StackSummaries[*].{ID: StackId, StackName: StackName}", response)
 
---
+++ helpers/text.py
@@ -0,0 +1,7 @@
+class helpers():
+    def print_separator(self, text):
+        print('\n' * 3)
+        print("#" * 75)
+        print(text)
+        print("#" * 75)
+        return
