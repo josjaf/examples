681a558122eba93a14cb9a7d3a40844ba093bb07
Josh Jaffe <josjaffe@amazon.com>
2018-12-18 23:10:27 -0500

    jmespath instances

---
+++ aws/jmespath/instances.py
@@ -0,0 +1,9 @@
+import boto3
+import jmespath
+
+session = boto3.session.Session()
+ec2 = session.client('ec2')
+
+response = ec2.describe_instances()
+instanceIds = jmespath.search("Reservations[].Instances[].InstanceId", response)
+print(instanceIds)
