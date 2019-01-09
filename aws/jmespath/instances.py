4fe4325fb14f9e6c8200e385a0c6fa5ca0548fb5
Josh Jaffe <josjaffe@amazon.com>
2019-01-08 22:49:27 -0500

    more jmespath examples

--- aws/jmespath/instances.py
+++ aws/jmespath/instances.py
@@ -7,3 +7,10 @@ ec2 = session.client('ec2')
 response = ec2.describe_instances()
 instanceIds = jmespath.search("Reservations[].Instances[].InstanceId", response)
 print(instanceIds)
+
+
+# running instance ids
+running_instance_ids = jmespath.search("Reservations[].Instances[?State.Name=='running'].InstanceId | []", response)
+
+#
+running_instances = jmespath.search("Reservations[].Instances[?State.Name=='running'].InstanceId | []", response)
