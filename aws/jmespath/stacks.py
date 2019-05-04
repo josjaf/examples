fb7b11b7ec5366c274acde3b6d035f6e887524a1
Josh Jaffe <josjaf@gmail.com>
2019-05-04 16:09:44 -0400

    more complex filter for stack status

--- aws/jmespath/stacks.py
+++ aws/jmespath/stacks.py
@@ -8,6 +8,16 @@ session = boto3.Session()
 cloudformation = session.client('cloudformation')
 
 response = cloudformation.list_stacks()
+
+paginator = cloudformation.get_paginator('list_stacks')
+response_iterator = paginator.paginate()
+response = {}
+response['StackSummaries'] = []
+for page in response_iterator:
+    for s in page['StackSummaries']:
+        response['StackSummaries'].append(s)
+#### get paginated data
+
 ids = jmespath.search("StackSummaries[].StackId", response)
 stacks = jmespath.search("StackSummaries[].StackName", response)
 
@@ -19,7 +29,12 @@ for stack in stacks:
 
 text_helpers.print_separator("Dictionary Example")
 
+running_instance_ids = jmespath.search("Reservations[].Instances[?State.Name=='running'].InstanceId | []", response)
+
 stacks = jmespath.search("StackSummaries[*].{ID: StackId, StackName: StackName}", response)
+stacks = jmespath.search("StackSummaries[?StackStatus=='CREATE_COMPLETE' "
+                         "|| StackStatus=='UPDATE_COMPLETE']"
+                         ".{ID: StackId, StackName: StackName, StackStatus: StackStatus}", response)
 
 for stack in stacks:
     print(stack)
