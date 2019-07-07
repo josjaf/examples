e09b1bf15242162662d06c4c424e22451fdb9def
Josh Jaffe <josjaf@gmail.com>
2019-07-07 12:12:40 -0400

    renaming

--- python/docker/docker_build/build.py
+++ python/docker/docker_build/build.py
@@ -14,7 +14,7 @@ def main():
 
     # TODO add exception processing for requests.exceptions.ConnectionError when docker dameon is not running
     #buildtime = t.strftime("%m-%d-%Y %H:%M:%S")
-    # commenting out the build time, because it will always generate a new image, but not take up more disks apce
+    # commenting out the docker_build time, because it will always generate a new image, but not take up more disks apce
     labels = {'Maintainer': 'josjaf', 'commit': sha}
     response = docker_client.images.build(path='.', tag='example:latest', labels=labels, dockerfile='Dockerfile')
     container_build_time = datetime.datetime.now() - t
