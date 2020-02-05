ec1cfee3fb762457221ed6312c2ad8dc637bb03f
Josh Jaffe <josjaf@gmail.com>
2020-02-05 13:48:02 -0500

    more updates

--- terraform/lambda_example/function.tf
+++ terraform/lambda_example/function.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "moo_organizations_lambda" {
   role             = aws_iam_role.lambda_role.arn
   handler          = "index.lambda_handler"
   source_code_hash = filebase64sha256(data.archive_file.moo.output_path)
-  runtime          = "python3.6"
+  runtime          = "python3.7"
 }
 
 resource "aws_lambda_function" "foo_organizations_lambda" {
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "foo_organizations_lambda" {
   role             = aws_iam_role.lambda_role.arn
   handler          = "index.lambda_handler"
   source_code_hash = filebase64sha256(data.archive_file.foo.output_path)
-  runtime          = "python3.6"
+  runtime          = "python3.7"
   environment {
     variables = {
       moo_function_name = aws_lambda_function.moo_organizations_lambda.function_name
