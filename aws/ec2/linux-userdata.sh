c6dc73d512b310afd53e85243e3ad288ba4d15f8
Josh Jaffe <josjaf@gmail.com>
2020-05-05 19:25:28 -0400

    more linux userdata

--- aws/ec2/linux-userdata.sh
+++ aws/ec2/linux-userdata.sh
@@ -7,4 +7,14 @@ response=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id
 export instance_name=$(echo $response | jq -r '.Tags[] | select(.Key=="Name") | .Value')
 
 export asg_name=$(echo $response | jq -r '.Tags[] | select(.Key=="aws:autoscaling:groupName") | .Value')
+
+# namespace with jq
 export namespace=$(echo $response | jq -r '.Tags[] | select(.Key=="namespace") | .Value')
+
+# namespace without jq
+export namespace=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" "Name=key,Values=namespace" --query 'Tags[0].Value' --output text)
+
+### install cloudwatch agent
+wget https://s3.${region}.amazonaws.com/amazoncloudwatch-agent-${region}/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+rpm -U ./amazon-cloudwatch-agent.rpm
+rm -rf amazon-cloudwatch-agent.rpm
