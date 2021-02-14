cf06050beaf3abc0c46a5ba6527ec2eaa6361fa7
Josh Jaffe <josjaf@gmail.com>
2021-02-14 12:48:54 -0500

    updating state file tf code for python

--- aws/s3/tf_state_file.py
+++ aws/s3/tf_state_file.py
@@ -1,16 +1,20 @@
 import boto3
-
+import string
+import random
+def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 def main():
     session = boto3.session.Session()
     s3 = session.client('s3')
-    bucket = 'josjaf-test'
+    bucket = f'josjaffe-gen-{id_generator(size=5)}'
     create_bucket_kwargs = dict(Bucket=bucket)
     if session.region_name != 'us-east-1':
         print("Adding Location Constraint to Payload")
         create_bucket_kwargs['CreateBucketConfiguration'] = {'LocationConstraint': session.region_name}
     try:
         response = s3.create_bucket(**create_bucket_kwargs)
+        print(f'Creating Bucket: {create_bucket_kwargs}')
     except: pass
     response = s3.put_bucket_versioning(
         Bucket=bucket,
