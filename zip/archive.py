8a4455ff82d7baec1a1e6947cfe1e02e9e032004
Josh Jaffe <josjaf@gmail.com>
2022-04-28 10:17:28 -0400

    adding archive file

--- zip/archive.py
+++ zip/archive.py
@@ -1,16 +1,25 @@
 import os
-from pathlib import Path
 from io import BytesIO
-from zipfile import ZipFile
-# go up one dir
-os.chdir('../')
-directory = Path.cwd()
-zip_path = Path.cwd().joinpath("archive.zip")
-print(f"Writing {zip_path}")
-with ZipFile(f"{zip_path}", mode="w") as archive:
-     for file_path in directory.rglob("**/*"):
+from zipfile import ZipFile, ZipInfo
 
-         if file_path.suffix in ['.zip', '*.pyc']: continue
-         if any(item in ['.git', '.eggs', '.idea', '__pycache__', ] for item in file_path.parts): continue
-         print(f"Writing {file_path}")
-         archive.write(file_path,arcname=file_path.relative_to(directory.parent))
+from pathlib import Path
+
+def in_memory_zip(directory: Path, working_directory='../'):
+    os.chdir(working_directory)
+    archive = BytesIO()
+    with ZipFile(archive, 'w') as zip_archive:
+        # Create three files on zip archive
+        for file_path in directory.rglob("**/*"):
+
+            if file_path.suffix in ['.zip', '*.pyc']: continue
+            if any(item in ['.git', '.eggs', '.idea', '__pycache__', ] for item in file_path.parts): continue
+            if file_path.is_dir(): continue
+            print(file_path)
+            file1 = ZipInfo(str(file_path.relative_to(Path.cwd())))
+            zip_archive.writestr(file1, file_path.read_bytes())
+    return archive
+if __name__ == '__main__':
+    directory = Path.cwd()
+    archive = in_memory_zip(directory, '../')
+    with open('config1.zip', 'wb') as f:
+        f.write(archive.getbuffer())
