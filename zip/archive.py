b8ac4391a0fad84e6c6d1a5041700113c1614b71
Josh Jaffe <josjaf@gmail.com>
2022-04-27 22:25:59 -0400

    adding archive file

---
+++ zip/archive.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+from io import BytesIO
+from zipfile import ZipFile
+os.chdir('../')
+directory = Path.cwd()
+zip_path = Path.cwd().joinpath("archive.zip")
+print(f"Writing {zip_path}")
+with ZipFile(f"{zip_path}", mode="w") as archive:
+     for file_path in directory.rglob("**/*"):
+
+         if file_path.suffix in ['.zip', '*.pyc']: continue
+         if any(item in ['.git', '.eggs', '.idea', '__pycache__', ] for item in file_path.parts): continue
+         print(f"Writing {file_path}")
+         archive.write(file_path,arcname=file_path.relative_to(directory.parent))
